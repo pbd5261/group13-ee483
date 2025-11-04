599303baf86b8e6949474b092af3f32d07b8994d
pbd5261 <pbd5261@psu.edu>
2025-11-04 17:50:06 -0500

    still error

--- lab-devel/packages/lab3/src/lanedetector.py
+++ lab-devel/packages/lab3/src/lanedetector.py
@@ -6,8 +6,8 @@ import matplotlib
 import numpy as np
 import os
 from message_filters import ApproximateTimeSynchronizer, Subscriber
-from duckietown_msgs.msg import Segment, SegmentList, Vector2D
-from sensor_msgs.msg import LanePose, Twist2DStamped
+from duckietown_msgs.msg import Segment, SegmentList, Vector2D, LanePose
+#from sensor_msgs.msgs import Twist2DStamped
 from cv_bridge import CvBridge
 from std_srvs.srv import SetBool, SetBoolResponse
 
@@ -17,18 +17,21 @@ class LaneDetector:
     def __init__(self):
         self.bridge = CvBridge()
         veh_name = os.environ['VEHICLE_NAME']
-        rospy.Subscriber(f"/{veh_name}/lane_filter_node/lane_pose", LanePose, self.processor, queue_size=1, buff_size= 2**24)
+        rospy.Subscriber(f"/{veh_name}/lane_filter_node/lane_pose", LanePose, self.callback, queue_size=1, buff_size= 2**24)
         rospy.pub_1 = rospy.Publisher('/ee483mm13/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
-        self.pub_phi = rospy.Publisher("avg_phi", LanePose, queue_size=32)
+        rospy.pub_phi = rospy.Publisher("avg_phi", LanePose, queue_size=32)
         car_cmd = Twist2DStamped()
         car_cmd.v = 0
         
  
-    def callback():
-        
+    def callback(self,LanePose):
+        self.movingavg(LanePose)
+
+
     def movingAvg(self, phi):
         newPhi = 0
-        for temp <= 5:
+        temp = 0
+        for temp in range(5):
             if temp < 5:
                 newPhi = newPhi + phi
             else:
